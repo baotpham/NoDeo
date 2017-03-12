aef5c46f101330d1d208a608b144c324c6c3e388
dagmawim <dmm474@drexel.edu>
2017-03-12 18:20:38 -0400

    Further restructure and discard comments

--- clientJS/login.js
+++ clientJS/login.js
@@ -1,28 +1,8 @@
 // Author: ..
 // Purpose: ..
-
 (function() {
-    // URL's that adress the services this application consumes
-    var baseURL = "http://localhost:8080"
-
     //Make sure DOM calls are made after the document is ready
     $(document).ready(function() {
         console.log('login.js HERE');
-        setEventHandlers();
     });
-
-    // Attach all the event handlers for the buttons
-    function setEventHandlers() {}
-
-    // use AJAX to make an HTTP get request to the service 
-    function makeRequest(type, endpt, data) {
-        var jqxhr =
-            $.ajax({
-                type: type,
-                url: baseURL + "/" + endpt,
-                dataType: "html",
-                data: data
-            });
-        return jqxhr
-    }
 })();
--- clientJS/main.js
+++ clientJS/main.js
@@ -1,34 +1,19 @@
 // Author : ...
 // Purpose : ...
-
 (function() {
-    // URL's that adress the services this application consumes
-    var baseURL = "http://localhost:8080"
 
     //Make sure DOM calls are made after the document is ready
     $(document).ready(function() {
+        console.log('login.js HERE');
         setEventHandlers();
     });
-
     // Attach all the event handlers for the buttons
-
     function setEventHandlers() {
         // $("#").click(function() {})
     }
-
-    // use AJAX to make an HTTP get request to the service 
-    function makeRequest(type, endpt, data) {
-        var jqxhr =
-            $.ajax({
-                type: type,
-                url: baseURL + "/" + endpt,
-                dataType: "html",
-                data: data
-            });
-        return jqxhr
-    }
-
 })();
+
+
 
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
