281d070ba54223c4c2f9b8b99d8469a3d1774549
bbueno24 <arcana.bueno@gmail.com>
2017-03-19 01:45:35 -0400

    Updated tables

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -2,32 +2,16 @@ DROP DATABASE IF EXISTS vnt;
 CREATE DATABASE vnt;
 USE vnt;
 
-# Users may have visited multiple videos
-CREATE TABLE access_log (
-	entry int NOT NULL AUTO_INCREMENT key,
-	username VARCHAR(20),
-	url VARCHAR(100)
+CREATE TABLE users (
+	userid INT KEY AUTO_INCREMENT,
+	username VARCHAR(20) UNIQUE NOT NULL, 
+    password VARCHAR(100) NOT NULL, 
+    type CHAR(1) DEFAULT 1
 );
 
-# Each video has a unique note id
-CREATE TABLE videos (
-	url VARCHAR(100) key,
-	note_id VARCHAR(100)
-);
-
-# Each id may have multiple notes
 CREATE TABLE notes (
-	note_id VARCHAR(100),
-	note VARCHAR(100) key
+	noteID INT KEY AUTO_INCREMENT,
+    username VARCHAR(20),
+    time TIME,
+    note TEXT
 );
-
-# Each note has a corresponding image
-CREATE TABLE images (
-	note VARCHAR(100) key,
-	image VARCHAR(100)
-);
-
-DESCRIBE access_log;
-DESCRIBE videos;
-DESCRIBE notes;
-DESCRIBE images;
